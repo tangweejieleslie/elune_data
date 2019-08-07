c6dcb61021e81e2e9ab1a48d4742c98ac722b83b
Leslie Tang <tangweejieleslie+github@gmail.com>
2019-08-07 10:11:38 +0800

    added initial template for data entry

--- elune-databoard/src/App.vue
+++ elune-databoard/src/App.vue
@@ -2,32 +2,24 @@
   <v-app>
     <v-app-bar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span>Elune</span>
+        <span class="font-weight-light">Data Dashboard</span>
       </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <DataForm/>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import DataForm from './components/DataForm';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    DataForm
   },
   data: () => ({
     //
